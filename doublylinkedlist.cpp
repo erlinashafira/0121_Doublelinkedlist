67e5bd435f0f33a84c4254eb5e12179526f3580d
errerew <erlina31acc@gmail.com>
2025-05-15 16:03:44 +0700

    menambahkan step 1 trnasverse list untuk menemukan node

--- doublylinkedlist.cpp
+++ doublylinkedlist.cpp
@@ -33,8 +33,10 @@ public:
         newNode->noMhs = nim;
 
         //step 3
-    if(START != NULL || nim << START->noMhs)
+    if(START == NULL || nim <= START->noMhs)
     {
+        if(START != NULL && nim == START->noMhs)
+
         cout << "\nDuplicate member not allowed" << endl;
         return;
     } 
@@ -86,7 +88,23 @@ public:
             return;
         }
 
-        
+        cout << "\nEnter the roll member of the student whose record is to be deleted"
+        int rollNo;
+        cin >> rollNo;
+
+        Node *current = START;
+
+        //step 1: Transverse the list to find the node
+        while(current != NULL && current->noMhs != rollNo)
+        {
+            current = current->next;
+        }
+
+        if (current == NULL)
+        {
+            cout << "Record not found" << endl;
+            return;
+        }
     }
 }
 }
